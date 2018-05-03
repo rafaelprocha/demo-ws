3a77d7ccfd908f0fcd0affdbc64ebd24fbf7672c
Rafael Rocha <rafael.p.rocha@gmail.com>
2018-05-03 13:57:01 -0300

    teste

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express();
 
-app.get('/teste', function(req, res) {
-	res.status(200).send("teste").end();
+app.post(
+	'/consulta-consumidor',
+	function(req, res) {
+		var responseXml = '<ROOT>'+
+			'<LTT CCC="0" CPF="11111111111" SIT="NE" NOM="NÃO CADASTRADO" DNT="1900-01-01" />' + 
+			'<DTH>0425130000</DTH>' + 
+			'<RCD>N000</RCD>' + 
+		    '<STM>00</STM>' + 
+		    '<RNR>0</RNR>' + 
+		    '<CST>0</CST>' +
+		    '<ICC>N</ICC>' +
+		'</ROOT>';
+		res.status(200).send(responseXml).end();
 });
 
 app.listen(8080);
